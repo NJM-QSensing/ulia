cd672b684a543235418e319ccaacc4edec9aa9ba
Daniel <daniel_uhl@hotmail.de>
2021-09-07 17:31:19 +0200

    Samlle Badge work.

--- ci/build_wheels.sh
+++ ci/build_wheels.sh
@@ -2,6 +2,6 @@
 
 python3 setup.py bdist_wheel
 
-pip install anybadge
+pip install anybadge numpy scipy numba
 pip install .
 anybadge -o --color "#006dad" -l PyPI -v $(python3 -c "import ulia; print(ulia.__version__)") -f pypi.svg
--- ulia/__init__.py
+++ ulia/__init__.py
@@ -3,3 +3,5 @@ from .routines import ULIA
 from .routines import cheb_bandpass_filter
 from .routines import diff_frequency_mixing
 from .routines import sum_frequency_mixing
+
+__version__ = '2021.09.1'
