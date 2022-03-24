9ef97577de07bdf0012e9fab2fda3c7ff48144b3
Daniel <daniel_uhl@hotmail.de>
2022-03-24 10:38:20 +0100

    Added butter bandpass filter to init file.

--- ulia/__init__.py
+++ ulia/__init__.py
@@ -2,8 +2,9 @@
 
 from .routines import ULIA
 from .routines import cheb_bandpass_filter
+from .routines import butter_bandpass_filter
 from .routines import diff_frequency_mixing
 from .routines import sum_frequency_mixing
 
 
-__version__ = '2021.12.1'
+__version__ = '2022.03.1'
